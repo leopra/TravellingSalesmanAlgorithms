81015ea7dec6fe7075d41b46bcb5e70f727dc1ce
leopra <leonardo.pratesi@studenti.unipd.it>
2020-05-13 22:47:06 +0200

    added recursive karp

--- HeldKarp.py
+++ HeldKarp.py
@@ -2,6 +2,9 @@ import utilities as ut
 import os 
 import sys
 import itertools as itol
+import time
+
+
 
 filename = sorted(os.listdir('tsp_dataset'))
 if (len(sys.argv)==2):
@@ -19,11 +22,9 @@ def calcHeldKarp(dmatrix):
 
     #inside C it saves all subset solutions
     C = { }
-
+    
     for i in range(1, dim):
         C[(i, 1 << i)] = (dmatrix[i,0], 0)
-
-    print(C)
 
     for j in range(2, dim):
         for subset in itol.combinations(range(1,dim), j):
@@ -36,7 +37,7 @@ def calcHeldKarp(dmatrix):
             for k in subset:
                 #remove k from S and search on all these possibile sets
                 prevsub = enc & ~ (1 << k)
-
+                
                 results= []
                 for m in subset:
                     #the 0 node is the starting point so is ignored, also if j point is already traversed is ignored
@@ -56,6 +57,7 @@ def calcHeldKarp(dmatrix):
     #now we search for the encoding containing all cities (0b11111...0) except the first
     final = 2**dim-2
     fresults = []
+    print('C: ', C)
 
     for k in range(1,dim):
         #for each node of final combination (all nodes except 0) check distance to 0
@@ -63,8 +65,81 @@ def calcHeldKarp(dmatrix):
     
     opt, parent = min(fresults)
 
-    #TODO missing path reconstruction
+    path = []
+    for i in range(dim - 1):
+        path.append(parent)
+        new_bits = final & ~(1 << parent)
+        _, parent = C[(parent, final)]
+        final = new_bits
+
+    print('il path: ', list(reversed(path)))
+    # Add implicit start state
+    path.append(0)
+
     return opt
+
+#bigger recursion limit to avoid error
+sys.setrecursionlimit(1000)
+
+#recursive version of the Held Karp algorithm
+#receives a matrix of distances and returns the min circular path traversing al nodes once
+def calcHeldKarpRecursiveVersion(dmatrix):
+    dim = len(dmatrix)-1 
+    dist = {}
+    py = {}   
+    
+    #recursive function to be called
+    def HK_Visit(v, S):
+        #print(v,encToList(S)) #Debug
+        #case base
+        if 1<<v == S:
+            return dmatrix[0,v]
+
+        #already calculated distance
+        elif (v,S) in dist.keys():
+            return dist[v,S]
+
+        else:
+            mindist = 2147483647
+            minprec = -1
+
+            prevsub = S & ~ (1 << v)
+            listanodi = encToList(prevsub)
+
+            for u in listanodi:
+                distance = HK_Visit(u, prevsub)
+
+                if distance + dmatrix[u,v] < mindist:
+                    mindist = distance + dmatrix[u,v]
+                    minprec = u
+            
+            dist[v,S] = mindist
+            py[v,S] = minprec
+
+            return mindist
+
+    def build_path():
+        percorso = []
+        k=0
+        S=2**dim-1
+        percorso.append(0)
+        while len(percorso) < len(dmatrix)-1:
+            temp = py[(k,S)]
+            percorso.append(temp)
+            S = S & ~ (1 << k)
+            k = temp
+
+        distance =0
+        # for i in range(0,len(percorso)-2):
+        #     distance += dmatrix[percorso[i+1],percorso[i]]
+        # print('DISTANZIONA: ', distance) 
+        return percorso
+
+
+    risultato = HK_Visit(0, 2**dim-1) #(1,V)
+    #indexes were used 1 off so 1 add one to all so they represent the real nodes name
+    print('il path: ', list(map(lambda x: x+1, build_path())))
+    return risultato
 
 
 #debugging function to see what sets are being considered
@@ -81,8 +156,23 @@ def printenc(C):
                     sett.append(str(num))
             print (key[0] , sett, ': ', value)
 
+#convert the binary encoding in the corresponding list
+def encToList(binary):
+    t = bin(binary)[2:]
+    sett = []
+    num=0
+    for i in t[::-1]:
+        if i == '1':
+            sett.append(num)
+        num += 1
+    return sett
 
-
-
-print(calcHeldKarp(a)) 
-
+#print(a)
+s = time.time()
+print('Iterative: ', calcHeldKarp(a))
+e = time.time()
+print(e-s)
+s = time.time()
+print('Recursive: ', calcHeldKarpRecursiveVersion(a)) 
+e = time.time()
+print(e-s)
